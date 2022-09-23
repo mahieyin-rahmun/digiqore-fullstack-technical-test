d07d3f890bda76e22a78adf22d740a0f9c143384
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2022-09-23 18:13:45 +0600

    chore: update dockerfile to download geckodriver automatically

--- server/entrypoint.dev.sh
+++ server/entrypoint.dev.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.31.0-linux64.tar.gz
+chmod +x geckodriver
+mv geckodriver /usr/local/bin/
+export PATH=$PATH:/usr/local/bin/geckodriver
+
 yarn migrate:dev
 yarn seed:dev
 yarn run:dev
