0a578856507e151f234ee3f3d18400bd375050f5
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2022-09-23 20:18:52 +0600

    feat: update selenium driver config

--- server/src/services/SeleniumDriverService.ts
+++ server/src/services/SeleniumDriverService.ts
@@ -1,3 +1,4 @@
+import { SELENIUM_CONTAINER_PORT, SELENIUM_HOST } from "@/config";
 import { injectable } from "inversify";
 import { Builder, ThenableWebDriver } from "selenium-webdriver";
 import firefox from "selenium-webdriver/firefox";
@@ -14,7 +15,17 @@ export class SeleniumDriverService {
     if (!this.driver) {
       this.driver = new Builder()
         .forBrowser("firefox")
-        .setFirefoxOptions(new firefox.Options().headless())
+        .usingServer(
+          `http://${SELENIUM_HOST!}:${SELENIUM_CONTAINER_PORT}/wd/hub`,
+        )
+        .setFirefoxOptions(
+          new firefox.Options()
+            .windowSize({
+              width: 640,
+              height: 480,
+            })
+            .headless(),
+        )
         .build();
     }
 
