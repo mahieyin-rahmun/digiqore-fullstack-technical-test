767904703b95ffdcc04f76f778bdaafa8a15fd5b
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2022-09-23 23:18:08 +0600

    feat(#15) add pagination support

--- client/components/table/LanguageLevelTable.tsx
+++ client/components/table/LanguageLevelTable.tsx
@@ -1,43 +1,166 @@
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { TableCellProps } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
 import Paper from "@mui/material/Paper";
 import { TTableProps } from "@/types";
-import { Typography } from "@mui/material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import React from "react";
+import {
+  BiSkipPreviousCircle,
+  BiSkipPrevious,
+  BiSkipNext,
+  BiSkipNextCircle,
+} from "react-icons/bi";
 
-const LanguageLevelTable: React.FC<TTableProps> = ({ data }) => {
+const StyledTableCell: React.FC<TableCellProps> = ({ children, ...props }) => {
+  const theme = useTheme();
   return (
-    <TableContainer
-      component={Paper}
+    <TableCell
+      {...props}
       sx={{
-        height: "60vh",
+        color: "white",
+        background: theme.palette.grey["A700"],
       }}
     >
+      {children}
+    </TableCell>
+  );
+};
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number,
+  ) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? (
+          <BiSkipNextCircle />
+        ) : (
+          <BiSkipPreviousCircle />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? <BiSkipNext /> : <BiSkipPrevious />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? <BiSkipPrevious /> : <BiSkipNext />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? (
+          <BiSkipPreviousCircle />
+        ) : (
+          <BiSkipNextCircle />
+        )}
+      </IconButton>
+    </Box>
+  );
+}
+
+const LanguageLevelTable: React.FC<TTableProps> = ({ data }) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
       <Table sx={{ height: "max-content" }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>
+            <StyledTableCell>
               <Typography variant="h6" fontWeight={"bold"}>
                 Language
               </Typography>
-            </TableCell>
-            <TableCell align="center">
+            </StyledTableCell>
+            <StyledTableCell align="center">
               <Typography variant="h6" fontWeight={"bold"}>
                 Level
               </Typography>
-            </TableCell>
-            <TableCell align="center">
+            </StyledTableCell>
+            <StyledTableCell align="center">
               <Typography variant="h6" fontWeight={"bold"}>
                 Average Source Statements
               </Typography>
-            </TableCell>
+            </StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => (
+          {(rowsPerPage > 0
+            ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : data
+          ).map((row) => (
             <TableRow
               key={row.id || row.language}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -56,6 +179,26 @@ const LanguageLevelTable: React.FC<TTableProps> = ({ data }) => {
             </TableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              colSpan={3}
+              count={data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  "aria-label": "rows per page",
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
